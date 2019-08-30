36f21c714007ec22cf8b21a6d42e9b1d8346c2aa
Alexey Egin <egin9@mail.ru>
2019-08-30 13:58:12 +0300

    feat: Improve work item model

--- models/azure/azureWorkItems.go
+++ models/azure/azureWorkItems.go
@@ -1,5 +1,7 @@
 package azure
 
+import "time"
+
 type ShortWorkItemsList struct {
 	ShortWorkItems []ShortWorkItem `json:"workItems"`
 }
@@ -21,7 +23,12 @@ type WorkItem struct {
 }
 
 type Fields struct {
-	SystemWorkItemType string `json:"System.WorkItemType"`
-	SystemReason       string `json:"System.Reason"`
-	SystemTitle        string `json:"System.Title"`
+	SystemWorkItemType                      string    `json:"System.WorkItemType"`
+	SystemState                             string    `json:"System.State"`
+	SystemReason                            string    `json:"System.Reason"`
+	SystemCreatedDate                       time.Time `json:"System.CreatedDate"`
+	SystemTitle                             string    `json:"System.Title"`
+	MicrosoftVSTSSchedulingOriginalEstimate float64   `json:"Microsoft.VSTS.Scheduling.OriginalEstimate"`
+	MicrosoftVSTSSchedulingCompletedWork    float64   `json:"Microsoft.VSTS.Scheduling.CompletedWork"`
+	MicrosoftVSTSCommonPriority             int       `json:"Microsoft.VSTS.Common.Priority"`
 }
--- models/workItem.go
+++ models/workItem.go
@@ -1,14 +1,22 @@
 package models
 
-import "Databriz-Meetings-API-Go/models/azure"
+import (
+	"Databriz-Meetings-API-Go/models/azure"
+	"time"
+)
 
 type WorkItem struct {
-	ID     int    `json:"id"`
-	Rev    int    `json:"rev"`
-	Type   string `json:"type"`
-	Reason string `json:"reason"`
-	Title  string `json:"title"`
-	URL    string `json:"url"`
+	ID               int       `json:"id"`
+	Rev              int       `json:"rev"`
+	Type             string    `json:"type"`
+	State            string    `json:"state"`
+	Reason           string    `json:"reason"`
+	CreationDate     time.Time `json:"creation_date"`
+	Title            string    `json:"title"`
+	OriginalEstimate float64   `json:"original_estimate"`
+	CompletedTime    float64   `json:"completed_time"`
+	Priority         int       `json:"priority"`
+	URL              string    `json:"url"`
 }
 
 func FromAzureWorkItems(azureWorkItemsResponse *azure.WorkItemsList) *[]WorkItem {
@@ -16,12 +24,17 @@ func FromAzureWorkItems(azureWorkItemsResponse *azure.WorkItemsList) *[]WorkItem
 
 	for index, workItem := range azureWorkItemsResponse.Value {
 		list[index] = WorkItem{
-			ID:     workItem.ID,
-			Rev:    workItem.Rev,
-			Type:   workItem.Fields.SystemWorkItemType,
-			Reason: workItem.Fields.SystemReason,
-			Title:  workItem.Fields.SystemTitle,
-			URL:    workItem.URL,
+			ID:               workItem.ID,
+			Rev:              workItem.Rev,
+			Type:             workItem.Fields.SystemWorkItemType,
+			State:            workItem.Fields.SystemState,
+			CreationDate:     workItem.Fields.SystemCreatedDate,
+			Reason:           workItem.Fields.SystemReason,
+			Title:            workItem.Fields.SystemTitle,
+			OriginalEstimate: workItem.Fields.MicrosoftVSTSSchedulingOriginalEstimate,
+			CompletedTime:    workItem.Fields.MicrosoftVSTSSchedulingCompletedWork,
+			Priority:         workItem.Fields.MicrosoftVSTSCommonPriority,
+			URL:              workItem.URL,
 		}
 	}
 
